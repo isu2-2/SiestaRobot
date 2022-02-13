67f0734d6bc329a05f254380a318dcfa984c8dbe
anant-goel <85389190+anant-goel@users.noreply.github.com>
2022-02-13 11:11:28 +0530

    Add files via upload

--- SiestaRobot/modules/google.py
+++ SiestaRobot/modules/google.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib
+from EzilaXBot import telethn as tbot
 import glob
 import io
 import os
@@ -10,7 +11,7 @@ from urllib.parse import urlencode
 import requests
 from bs4 import BeautifulSoup
 from PIL import Image
-from search_engine_parser.core.engines.google import Search as GoogleSearch
+from search_engine_parser import GoogleSearch
 
 import bs4
 import html2text
@@ -20,11 +21,9 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from SiestaRobot import *
+from EzilaXBot import *
 
-from SiestaRobot.events import register
-from SiestaRobot import telethn as tbot
-from SiestaRobot.modules.language import gs
+from Sophia.events import register
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
@@ -35,7 +34,7 @@ opener.addheaders = [("User-agent", useragent)]
 async def _(event):
     if event.fwd_from:
         return
-
+    
     webevent = await event.reply("searching........")
     match = event.pattern_match.group(1)
     page = re.findall(r"page=\d+", match)
@@ -61,12 +60,11 @@ async def _(event):
         "**Search Query:**\n`" + match + "`\n\n**Results:**\n" + msg, link_preview=False
     )
 
-
-@register(pattern="^/image (.*)")
+@register(pattern="^/img (.*)")
 async def img_sampler(event):
     if event.fwd_from:
         return
-
+    
     query = event.pattern_match.group(1)
     jit = f'"{query}"'
     downloader.download(
@@ -94,10 +92,10 @@ opener.addheaders = [("User-agent", useragent)]
 
 @register(pattern=r"^/reverse(?: |$)(\d*)")
 async def okgoogle(img):
-    """For .reverse command, Google search images and stickers."""
+    """ For .reverse command, Google search images and stickers. """
     if os.path.isfile("okgoogle.png"):
         os.remove("okgoogle.png")
-
+    
     message = await img.get_reply_message()
     if message and message.media:
         photo = io.BytesIO()
@@ -208,77 +206,20 @@ async def scam(results, lim):
     return imglinks
 
 
-@register(pattern="^/app (.*)")
-async def apk(e):
-
-    try:
-        app_name = e.pattern_match.group(1)
-        remove_space = app_name.split(" ")
-        final_name = "+".join(remove_space)
-        page = requests.get(
-            "https://play.google.com/store/search?q=" + final_name + "&c=apps"
-        )
-        lnk = str(page.status_code)
-        soup = bs4.BeautifulSoup(page.content, "lxml", from_encoding="utf-8")
-        results = soup.findAll("div", "ZmHEEd")
-        app_name = (
-            results[0].findNext("div", "Vpfmgd").findNext("div", "WsMG1c nnK0zc").text
-        )
-        app_dev = results[0].findNext("div", "Vpfmgd").findNext("div", "KoLSrc").text
-        app_dev_link = (
-            "https://play.google.com"
-            + results[0].findNext("div", "Vpfmgd").findNext("a", "mnKHRc")["href"]
-        )
-        app_rating = (
-            results[0]
-            .findNext("div", "Vpfmgd")
-            .findNext("div", "pf5lIe")
-            .find("div")["aria-label"]
-        )
-        app_link = (
-            "https://play.google.com"
-            + results[0]
-            .findNext("div", "Vpfmgd")
-            .findNext("div", "vU6FJ p63iDd")
-            .a["href"]
-        )
-        app_icon = (
-            results[0]
-            .findNext("div", "Vpfmgd")
-            .findNext("div", "uzcko")
-            .img["data-src"]
-        )
-        app_details = "<a href='" + app_icon + "'>📲&#8203;</a>"
-        app_details += " <b>" + app_name + "</b>"
-        app_details += (
-            "\n\n<code>Developer :</code> <a href='"
-            + app_dev_link
-            + "'>"
-            + app_dev
-            + "</a>"
-        )
-        app_details += "\n<code>Rating :</code> " + app_rating.replace(
-            "Rated ", "⭐ "
-        ).replace(" out of ", "/").replace(" stars", "", 1).replace(
-            " stars", "⭐ "
-        ).replace(
-            "five", "5"
-        )
-        app_details += (
-            "\n<code>Features :</code> <a href='"
-            + app_link
-            + "'>View in Play Store</a>"
-        )
-        app_details += "\n\n===> Siesta <==="
-        await e.reply(app_details, link_preview=True, parse_mode="HTML")
-    except IndexError:
-        await e.reply("No result found in search. Please enter **Valid app name**")
-    except Exception as err:
-        await e.reply("Exception Occured:- " + str(err))
 
 
-def helps(chat):
-    return gs(chat, "search_help")
 
+__mod_name__ = "💞 Google 💞"
 
-__mod_name__ = "Search"
+__help__ = """
+ ~ /google <text>*:* Perform a google search
+ ~ /img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
+ ~ /reverse: Does a reverse image search of the media which it was replied to.
+ ~ /gps <location>*:* Get gps location.
+ ~ /github <username>*:* Get information about a GitHub user.
+ ~ /country <country name>*:* Gathering info about given country
+ ~ /imdb <Movie name>*:* Get full info about a movie with imdb.com
+ ~ Yone <query>*:* Yone answers the query
+
+  😜Ex: `Yone where is Sri lanka?`
+"""
