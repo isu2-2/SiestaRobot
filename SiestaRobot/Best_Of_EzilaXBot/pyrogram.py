dcdd49fb97671cee38017c7255f642f0640c2e06
anant-goel <85389190+anant-goel@users.noreply.github.com>
2022-02-13 11:23:32 +0530

    Update pyrogram.py

--- SiestaRobot/Best_Of_EzilaXBot/pyrogram.py
+++ SiestaRobot/Best_Of_EzilaXBot/pyrogram.py
@@ -3,13 +3,13 @@ import logging
 from pyrogram import Client
 
 # from pyromod import listen
-from EzilaXBot.conf import get_int_key, get_str_key
+from SiestaRobot.conf import get_int_key, get_str_key
 
 TOKEN = get_str_key("TOKEN", required=True)
 API_ID = get_int_key("API_ID", required=True)
 API_HASH = get_str_key("API_HASH", required=True)
 session_name = TOKEN.split(":")[0]
-EzilaXBot = Client(
+SiestaRobot = Client(
     session_name,
     api_id=API_ID,
     api_hash=API_HASH,
@@ -19,4 +19,4 @@ EzilaXBot = Client(
 # disable logging for pyrogram [not for ERROR logging]
 logging.getLogger("pyrogram").setLevel(level=logging.ERROR)
 
-EzilaXBot.start()
+SiestaRobot.start()
