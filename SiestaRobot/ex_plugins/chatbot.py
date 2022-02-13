305da97ea19a556b4b7e8c09aef4357d513256ae
anant-goel <85389190+anant-goel@users.noreply.github.com>
2022-02-13 12:59:52 +0530

    Update chatbot.py

--- SiestaRobot/ex_plugins/chatbot.py
+++ SiestaRobot/ex_plugins/chatbot.py
@@ -1,33 +1,37 @@
 from SiestaRobot.mongo import client as db_x
 
-lydia = db_x["CHATBOT"]
+lydia = db_x["CAHTBOT"]
 
 
 def add_chat(chat_id):
     stark = lydia.find_one({"chat_id": chat_id})
     if stark:
         return False
-    lydia.insert_one({"chat_id": chat_id})
-    return True
+    else:
+        lydia.insert_one({"chat_id": chat_id})
+        return True
 
 
 def remove_chat(chat_id):
     stark = lydia.find_one({"chat_id": chat_id})
     if not stark:
         return False
-    lydia.delete_one({"chat_id": chat_id})
-    return True
+    else:
+        lydia.delete_one({"chat_id": chat_id})
+        return True
 
 
 def get_all_chats():
     r = list(lydia.find())
     if r:
         return r
-    return False
+    else:
+        return False
 
 
 def get_session(chat_id):
     stark = lydia.find_one({"chat_id": chat_id})
     if not stark:
         return False
-    return stark
+    else:
+        return stark
